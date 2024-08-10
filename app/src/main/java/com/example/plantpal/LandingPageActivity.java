3b58152a80ccd94d07d543c39b4f9a2aace16ab3
HenryTheNewbie <37680257+HenryTheNewbie@users.noreply.github.com>
2024-08-10 20:40:57 +0800

    Revert "Commit #6"

--- app/src/main/java/com/example/plantpal/LandingPageActivity.java
+++ app/src/main/java/com/example/plantpal/LandingPageActivity.java
@@ -1,23 +1,13 @@
 package com.example.plantpal;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.view.Menu;
 import android.widget.Button;
-import android.view.MenuItem;
-import android.widget.ImageView;
-import android.widget.TextView;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-import androidx.annotation.NonNull;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LandingPageActivity extends AppCompatActivity {
 
     private Button temporaryBackButton;
-
-    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,29 +16,5 @@ public class LandingPageActivity extends AppCompatActivity {
 
         temporaryBackButton = findViewById(R.id.temporary_back_button);
         temporaryBackButton.setOnClickListener(view -> finish());
-
-        bottomNavigationView = findViewById(R.id.navigation_bar);
-        bottomNavigationView.setSelectedItemId(R.id.dummy_item);
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                int itemId = item.getItemId();
-                if (itemId == R.id.nav_plant_scan) {
-                    // Handle Plant Scan action
-                    return true;
-                } else if (itemId == R.id.nav_plant_archive) {
-                    // Handle Plant Archive action
-                    return true;
-                } else if (itemId == R.id.nav_community_hub) {
-                    // Handle Community Hub action
-                    return true;
-                } else if (itemId == R.id.nav_my_garden) {
-                    // Handle My Garden action
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        });
     }
 }
