1204794b84a972c20a739d181c87b8ed1440b2cf
HenryTheNewbie <37680257+HenryTheNewbie@users.noreply.github.com>
2024-08-11 21:36:00 +0800

    Commit #7 - Implemented the redirection functionality for the bottom navigation bar. - Streamlined names for a few .xml files.

---
+++ app/src/main/java/com/example/plantpal/MyGardenActivity.java
@@ -0,0 +1,62 @@
+package com.example.plantpal;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MyGardenActivity extends AppCompatActivity {
+
+    private Button temporaryBackButton;
+
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scan_page);
+
+        temporaryBackButton = findViewById(R.id.temporary_back_button);
+        temporaryBackButton.setOnClickListener(view -> finish());
+
+        bottomNavigationView = findViewById(R.id.navigation_bar);
+
+        int selectedItemId = getIntent().getIntExtra("selected_item_id", -1);
+
+        if (selectedItemId != -1) {
+            bottomNavigationView.setSelectedItemId(selectedItemId);
+        }
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {@Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.nav_plant_scan) {
+                    Intent intent = new Intent(MyGardenActivity.this, ScanActivity.class);
+                    intent.putExtra("selected_item_id", itemId);
+                    startActivity(intent);
+                    return true;
+                } else if (itemId == R.id.nav_plant_archive) {
+                    Intent intent = new Intent(MyGardenActivity.this, PlantArchiveActivity.class);
+                    intent.putExtra("selected_item_id", itemId);
+                    startActivity(intent);
+                    return true;
+                } else if (itemId == R.id.nav_community_hub) {
+                    Intent intent = new Intent(MyGardenActivity.this, CommunityForumActivity.class);
+                    intent.putExtra("selected_item_id", itemId);
+                    startActivity(intent);
+                    return true;
+                } else if (itemId == R.id.nav_my_garden) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        });
+    }
+}
